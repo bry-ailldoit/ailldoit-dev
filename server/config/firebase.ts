20bf739e93699c0d0aa7dd25c51b714404c791de
bryailldoit <bry@ailldoit.com>
2025-11-19 03:53:54 +0800

    explicit dotenv: server/config/firebase

--- server/config/firebase.ts
+++ server/config/firebase.ts
@@ -1,3 +1,8 @@
+import { config } from 'dotenv';
+
+// Load environment variables
+config();
+
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getStorage } from 'firebase-admin/storage';
